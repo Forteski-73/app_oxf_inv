0145173626460f48f542d2b55c6689d02cc3000f
Diones Forteski <dionesforteski@gmail.com>
2024-12-06 16:22:08 -0300

    Implementação do menu inicial

---
+++ lib/menu/menu.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import '../pages/export.dart';
+import '../pages/import.dart';
+import '../pages/management.dart';
+import '../pages/settings.dart';
+import '../pages/sync.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData.dark(),
+    home: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MenuPage(),
+    );
+  }
+}
+
+class MenuPage extends StatelessWidget {
+  const MenuPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) { 
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Menu', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.black,
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              _menuItem(
+                context,
+                icon: Icons.inventory_2_outlined,
+                label: "Gerenciamento de Inventário",
+                page: const InventoryPage(), // Página de Gerenciamento de Inventário
+              ),
+              _menuItem(
+                context,
+                icon: Icons.upload_outlined,
+                label: "Exportação de Dados",
+                page: const ExportPage(), // Página de Exportação de Dados
+              ),
+              _menuItem(
+                context,
+                icon: Icons.download_outlined,
+                label: "Importação de Produtos",
+                page: const ImportPage(), // Página de Importação de Produtos
+              ),
+              _menuItem(
+                context,
+                icon: Icons.sync_outlined,
+                label: "Sincronização",
+                page: const SyncPage(), // Página de Sincronização
+              ),
+              _menuItem(
+                context,
+                icon: Icons.settings_outlined,
+                label: "Configurações",
+                page: const SettingsPage(), // Página de Configurações
+              ),
+            ],
+          ),
+
+          // Rodapé
+          Container(
+            color: Colors.grey[200],
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "Oxford Porcelanas",
+                  style: TextStyle(fontSize: 14),
+                ),
+                Text(
+                  "Versão: 1.0",
+                  style: TextStyle(fontSize: 14),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+// Corrigindo a assinatura da função _menuItem
+  Widget _menuItem(BuildContext context, {
+    required IconData icon,
+    required String label,
+    required Widget page,
+  }) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.black),
+      title: Text(label, style: TextStyle(color: Colors.black)),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => page),
+        );
+      },
+    );
+  }
+
+}
---
+++ lib/pages/sync.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class SyncPage extends StatelessWidget {
+  const SyncPage({Key? key}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Configurações', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.black,
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      body: const Center(child: Text('Conteúdo de Sincronização')),
+
+      // Rodapé
+      bottomNavigationBar: Container(
+        color: Colors.grey[200],
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Text(
+              "Oxford Porcelanas",
+              style: TextStyle(fontSize: 14),
+            ),
+            Text(
+              "Versão: 1.0",
+              style: TextStyle(fontSize: 14),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData.dark(), 
+    home: SyncPage(),
+  ));
+}
